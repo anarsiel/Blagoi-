85604ff3048198f8a79e04ef726c93841a46df32
Blagosha <dimitrovblagoi@gmail.com>
2020-05-04 01:20:29 +0300

    swiched to command line filename input

--- runRPAF.py
+++ runRPAF.py
@@ -5,8 +5,8 @@ from body.Interpreter import Interpreter
 
 interpreter = Interpreter()
 try:
-    # interpreter.parse_file(sys.argv[1])
-    interpreter.parse_file("main.rpaf")
+    interpreter.parse_file(sys.argv[1])
+    # interpreter.parse_file("main.rpaf")
 except (Interpreter.CompilationError, Interpreter.RunTimeError) as exception:
     logging.error(str(exception))
 except Interpreter.RunTimeWarning as exception:
